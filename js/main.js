129d643d8b0241cccaf167b8cdc6573fc2bd23ae
kano011 <kana.t0152u@gmail.com>
2020-07-10 23:32:27 +0900

    fix: src & href path add: smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
-})
+$(function () {
+  $('a[href^="#top"]').click(function () {
+    var speed = 500;
+    var adjust = 80;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - adjust;
+    $('html, body').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+});
